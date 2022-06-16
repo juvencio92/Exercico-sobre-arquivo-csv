71eb57abc13850961f58091c948805c63225c997
juvencio92 <jorgecamassadila1@gmail.com>
2022-06-16 18:58:50 +0100

    Duas propostas de resolulação

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ mostrar_dados = csv.reader(open("dados.csv"), delimiter=",")
 
 for [nome, idade, genero] in mostrar_dados:
     print("{} tem {} anos de idade e {}\n".format(nome, idade, genero))
+
+###################### OUTRA FORMA ##############################
+
+with open("dados.csv", "r") as arq:
+    leitura = csv.reader(arq, delimiter=",")
+    for [nome, idade, genero] in leitura:
+        print("{} is {} years old and {} ".format(nome, idade,genero))
